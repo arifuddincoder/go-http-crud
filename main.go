e7faf3df9cbb8637630cd4f08a0c2621d5867c72
arifucoder <arifucoder@gmail.com>
2026-05-20 06:29:27 +0600

    feat: add server start code

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", rootHandler)
+
+	fmt.Println("Server is running on 5000")
+
+	err := http.ListenAndServe(":5000", mux)
+
+	if err != nil {
+		fmt.Println("Server error", err)
+	}
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome to go server!")
 }
